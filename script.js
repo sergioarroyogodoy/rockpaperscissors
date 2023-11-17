6f71749f42c06e5e8feac553bff0634788c9dc7d
Sergio Arroyo <sergio_arroyo@live.com>
2023-11-16 23:00:03 -0600

    Explain in pseudocode of determining winner

---
+++ script.js
@@ -0,0 +1,6 @@
+// COMPUTE a random computer choice of either "rock", "paper", or "scissors"
+// OBTAIN "rock", "paper", or "scissors" input from user
+// DETERMINE the winner by analyzing who wins
+    // If the matchup is "rock" vs "paper", THEN "paper" wins
+    // IF the matchup is "rock" vs "scissors", THEN "rock" wins
+    // If the matchup is "paper" vs "scissors", THEN "scissors" wins
