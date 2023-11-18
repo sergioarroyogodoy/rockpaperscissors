43dafc3e1ab2fa6edfd02158365d068e507e08e2
Sergio Arroyo <sergio_arroyo@live.com>
2023-11-18 13:12:20 -0600

    Translate random number into computer's choice

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 //GENERATE a random choice of "rock", "paper", or "scissors" to be used as the computer's play.
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3);
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
 }
+
+console.log(getComputerChoice())
