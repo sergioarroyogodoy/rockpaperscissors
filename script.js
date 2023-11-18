2902ffb291ed65ed36b26e528e0c41bb7641b72e
Sergio Arroyo <sergio_arroyo@live.com>
2023-11-18 14:22:24 -0600

    Create a function to play a full 5 round game

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//GENERATE a random choice of "rock", "paper", or "scissors" to be used as the computer's play.
+//GENERATE a random choice of "rock", "paper", or "scissors" to be used as the computer's play
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
@@ -11,6 +11,14 @@ function getComputerChoice() {
     }
 }
 
+// PROMPT the user to provide us with their play
+function getUserChoice() {
+    let option = prompt("Rock, paper, scissors?").toLowerCase();
+    return option;
+}
+
+// DETERMINE if the outcome of the matchup is won, lost or drawn
+// OUTPUT a message letting the user know the outcome
 function playRound(playerSelection, computerSelection) {    
     let draw = () => "It's a draw!";
     let win = (playerSelection, computerSelection) => `You win, ${playerSelection} beats ${computerSelection}.`;
@@ -35,9 +43,20 @@ function playRound(playerSelection, computerSelection) {
                 return win(playerSelection, computerSelection);
             }
             else {return lose(playerSelection, computerSelection);}
+        default:
+            return "You did not enter a valid option."
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, "paper"));
+//REPEAT the game 5 times
+//INCREMENT a number depending on the number of wins, loses and draws
+//DETERMINE a winner at the end of the game
+function game () {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getUserChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game();
